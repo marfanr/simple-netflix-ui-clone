bb9f9ee000027d02f37c1b02811d882b18d57bd2
marfanr <mohammadarfan45@gmail.com>
2022-05-26 19:28:07 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,200 @@
+// Netflix UI Clone with flutter
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: Theme.of(context).copyWith(
+          scaffoldBackgroundColor: const Color(0xFF151515),
+          textTheme: const TextTheme(bodyText1: TextStyle(color: Colors.white, fontSize: 16))),
+      debugShowCheckedModeBanner: false,
+      title: 'Netflix UI Clone',
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Home extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomeState extends State<Home> {
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        extendBodyBehindAppBar: true,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => print('cast button clicked'),
+          backgroundColor: Colors.black26.withOpacity(0),
+          child: const Icon(Icons.cast),
+        ),
+        bottomNavigationBar: Container(
+          height: 55,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(color: Colors.grey.withOpacity(0.3), blurRadius: 1, spreadRadius: 1),
+            ],
+            color: const Color(0xFF151515),
+          ),
+        ),
+        appBar: PreferredSize(
+          preferredSize: Size(MediaQuery.of(context).size.width, 60),
+          child: Container(
+            color: Colors.black45.withOpacity(0),
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            width: MediaQuery.of(context).size.width,
+            child: SafeArea(
+                child: Row(
+              children: [
+                const Image(image: AssetImage('assets/logo.png'), width: 40),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisSize: MainAxisSize.max,
+                    children: const [
+                      Text('Acara TV'),
+                      Text('Film'),
+                      Text('Daftar Saya'),
+                    ],
+                  ),
+                )
+              ],
+            )),
+          ),
+        ),
+        body: CustomScrollView(
+          controller: _scrollController,
+          slivers: [
+            SliverToBoxAdapter(
+                child: Container(
+              height: 400,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/poster.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Stack(
+                children: [
+                  Positioned(
+                    width: 130,
+                    bottom: 170,
+                    left: 140,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const Image(
+                          image: AssetImage('assets/logo.png'),
+                          width: 15,
+                        ),
+                        Text(
+                          'SERIAL',
+                          style: TextStyle(
+                              fontSize: 18, color: Colors.grey[400], letterSpacing: 5, fontWeight: FontWeight.w800),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Positioned(
+                    bottom: 70,
+                    left: 100,
+                    child: Text('STRANGER\nTHINGS',
+                        style: TextStyle(fontSize: 40, fontWeight: FontWeight.w700), textAlign: TextAlign.center),
+                  ),
+                  Positioned(
+                      bottom: 40,
+                      height: 20,
+                      left: 90,
+                      width: 220,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: const [
+                          Text('Mencekam'),
+                          Icon(
+                            Icons.circle,
+                            size: 5,
+                            color: Colors.red,
+                          ),
+                          Text('Seru'),
+                          Icon(
+                            Icons.circle,
+                            size: 5,
+                            color: Colors.red,
+                          ),
+                          Text('Remaja'),
+                        ],
+                      )),
+                ],
+              ),
+            )),
+            SliverToBoxAdapter(
+              child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: const EdgeInsets.all(10),
+//                  color: Colors.blue,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: const [
+                          Icon(Icons.add, color: Colors.white, size: 40),
+                          SizedBox(height: 5),
+                          Text('Daftar Saya'),
+                        ],
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        width: 120,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(5)),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: const [
+                            Icon(Icons.play_arrow),
+                            Text('Putar',
+                                style: TextStyle(color: Colors.black, fontWeight: FontWeight.w600, fontSize: 20)),
+                          ],
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: const [
+                          Icon(Icons.info_outline, color: Colors.white, size: 40),
+                          SizedBox(height: 5),
+                          Text('Info'),
+                        ],
+                      ),
+                    ],
+                  )),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            SliverToBoxAdapter(
+              child: Container(
+                padding: const EdgeInsets.all(15),
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Preview',
+                      style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        ));
   }
 }
